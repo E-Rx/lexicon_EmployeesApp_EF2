6fee0be0cbcf3b8f0e458949e3ef8cea739dd369
E-Rx <elise.rochaix@gmail.com>
2025-06-03 13:49:04 +0200

    Company entity added and database update

---
+++ EmployeesApp.Domain/Entities/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Domain.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+      
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
